61731b7a97c31972fef6a2a9469a32a14cacb2ae
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-07-26 10:32:57 +0300

    update

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-const site_1 = prompt("Enter 1st site");
-const site_2 = prompt("Enter 2st site");
-
-document.getElementById("button-1").addEventListener("click", () => {
-  if(site_1.indexOf("https://") != -1 || site_1.indexOf("http://") != -1){
-    window.open(site_1)
+function openSite(url) {
+  if (url.indexOf("https://") != -1 || url.indexOf("http://") != -1) {
+    window.open(url);
   }
-  const newSite_1 = "https://" + site_1
-  window.open(newSite_1)
-})
-
-document.getElementById("button-2").addEventListener("click", () => {
-  if(site_2.indexOf("https://") != -1 || site_1.indexOf("http://") != -1){
-    window.open(site_2)
-  }
-  const newSite_2 = "https://" + site_2
-  window.open(newSite_2)
-})
+  const newUrl = "http://" + url;
+  window.open(newUrl);
+}
